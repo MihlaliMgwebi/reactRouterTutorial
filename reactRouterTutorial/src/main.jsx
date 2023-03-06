0d9ac6651c17f4422f94fd825a37a3e66e21eaac
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-06 19:44:09 +0200

    feat: :construction: add router

--- reactRouterTutorial/src/main.jsx
+++ reactRouterTutorial/src/main.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
+const router = createBrowserRouter([
+  // [createBrowserRouter](https://reactrouter.com/en/main/routers/create-browser-router)
+  {
+    //root route: root layout of the UI
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
