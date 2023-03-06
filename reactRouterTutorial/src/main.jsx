8b3cca1a635276436d0af46b45c85020a160d4d0
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-06 20:22:05 +0200

    feat: :construction: Creating Contacts via Form
    <Form> prevents the browser from sending the request to the server and sends it to your route action instead. In web semantics, a POST usually means some data is changing. By convention, React Router uses this as a hint to automatically revalidate the data on the page after the action finishes. That means all of your useLoaderData hooks update and the UI stays in sync with your data automatically

--- reactRouterTutorial/src/main.jsx
+++ reactRouterTutorial/src/main.jsx
@@ -4,7 +4,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
 import Contact from "./routes/contact";
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, {
+  action as rootAction,
+  loader as rootLoader,
+} from "./routes/root";
 
 // [createBrowserRouter](https://reactrouter.com/en/main/routers/create-browser-router)
 const router = createBrowserRouter([
@@ -14,6 +17,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         // contact route : nested route
--- reactRouterTutorial/src/routes/root.jsx
+++ reactRouterTutorial/src/routes/root.jsx
@@ -1,15 +1,21 @@
 // root layout component
 
 import React from "react";
-import { Link, Outlet, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { Form, Link, Outlet, useLoaderData } from "react-router-dom";
+import { createContact, getContacts } from "../contacts";
 
 export async function loader() {
   const contacts = await getContacts();
   return { contacts };
 }
 
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
+
 export default function Root() {
+  // @ts-ignore
   const { contacts } = useLoaderData();
   return (
     <>
@@ -35,9 +41,9 @@ export default function Root() {
               className='sr-only'
               aria-live='polite'></div>
           </form>
-          <form method='post'>
+          <Form method='post'>
             <button type='submit'>New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {
