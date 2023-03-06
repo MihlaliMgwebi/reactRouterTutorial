38f6692be2c1f3829e6960340bbf41686e5a1df8
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-06 19:55:29 +0200

    feat: :goal_net: Create an error page component
    Anytime your app throws an error while rendering, loading data, or performing data mutations, React Router will catch it and render an error screen. Let's make our own error page.

--- reactRouterTutorial/src/main.jsx
+++ reactRouterTutorial/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
 import "./index.css";
 import Root from "./routes/root";
 
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
     //root route: root layout of the UI
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
